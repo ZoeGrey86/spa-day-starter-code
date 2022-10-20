a9cbb793ad67ece06f19e19b6460b152a9ab65df
Zoe Smith <107585629+ZoeGrey86@users.noreply.github.com>
2022-10-19 21:53:37 -0500

    user add form is functioning

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,12 +1,24 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class User {
+    @NotBlank(message = "user name required")
+    @Size(min = 5, max = 15, message = "user name must be between 5 and 15 characters")
     private String username;
+    @Email(message = "Invalid Email try again")
     private String email;
+    @NotBlank(message = "password required")
+    @Size(min = 6)
     private String password;
+    private int id;
+    private static int nextId = 1;
 
     public User() {
-
+        this.id = nextId;
+        nextId++;
     }
 
     public User(String username, String email, String password) {
@@ -38,5 +50,9 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public int getId() {
+        return id;
     }
 }
